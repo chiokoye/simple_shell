ec22bd653a9635ceb0c918f25b46abbf33a0344d
chiokoye <gennyokoye@gmail.com>
2023-10-13 21:01:23 -0700

    simple_shell

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+ /**
+ *  add -  This is t add two integers.
+ *  @num1: This is the first integer that is going to be added
+ *  @num2: This is the second integer that is going to be added
+ *
+ *  Return: This will give the result of the addition.
+ */
+
+ int add(int num1, int num2)
+ {
+	return (num1 + num2);
+ }
